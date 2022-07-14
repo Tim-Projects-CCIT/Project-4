c561d79f1722302db631dbb0d651b1490b0649a1
Rifaldlee <103484674+Rifaldlee@users.noreply.github.com>
2022-07-14 13:45:45 +0700

    Add files via upload

---
+++ Project_4/src/java/Model/ProductModel.java
@@ -0,0 +1,32 @@
+package Model;
+
+public class ProductModel {
+       
+    private String name;
+    private String price;
+    private String category;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
